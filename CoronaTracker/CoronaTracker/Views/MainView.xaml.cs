50fcb1048093f0d73a1662a9e6b942366b5d7af4
Peter Milovcik <peter.milovcik@gmail.com>
2020-03-19 11:56:38 +0100

    Update when MainView appears

--- CoronaTracker/CoronaTracker/Views/MainView.xaml.cs
+++ CoronaTracker/CoronaTracker/Views/MainView.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using CoronaTracker.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,15 @@ namespace CoronaTracker.Views
         public MainView()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            if (BindingContext is MainViewModel viewModel)
+            {
+                viewModel.UpdateCommand.Execute(null);
+            }
         }
     }
 }
